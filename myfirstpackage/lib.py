87c98b67af033d813f5b5a052c7f83a145ff07f1
Konstantin Thiel <konstantin_thiel@live.de>
2021-05-24 15:22:32 +0200

    Initial Push

---
+++ myfirstpackage/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    print('What is my second name?')
+    name = input()
+    
+    if name == 'Alexander':
+        print('Awesome job')
+    else:
+        print("You need to guess again, a big square in Berlin is named like it")
